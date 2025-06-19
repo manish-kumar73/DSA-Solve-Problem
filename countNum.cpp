4ef9b23a695512bcf55e56fb2a6e2665f80ad3b4
manish kumar <manishkumarr2060@gmail.com>
2025-06-20 02:29:42 +0530

    add:palindrome:code

--- countNum.cpp
+++ countNum.cpp
@@ -10,10 +10,32 @@ int contNumber(int num){
     return count;
 }
 
+bool palindromNumber(int num){
+    int reverse = 0;
+    int temp = num;
+    while(temp != 0){
+        int lastDigit = temp % 10;
+        reverse = (reverse * 10) + lastDigit;
+        temp /= 10;
+    }
+    if(reverse == num){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 int main(){
     int num;
     cout<<"enter the number of Num ";
     cin>>num;
 
-    cout<<contNumber(num);
+    //cout<<contNumber(num);
+
+    int ans = palindromNumber(num);
+    if(ans){
+        cout<<"Palindrome Number";
+    }else{
+        cout<<"Not Palindrome Number";
+    }
 }
