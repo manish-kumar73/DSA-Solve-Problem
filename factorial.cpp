cb86b8935cd274660123290343e1b7fd4315fe6a
manish kumar <manishkumarr2060@gmail.com>
2025-06-21 02:36:19 +0530

    done:factZeroCount

--- factorial.cpp
+++ factorial.cpp
@@ -8,6 +8,19 @@ int recursionFactorial(int num){
     return num * recursionFactorial(num - 1);
 }
 
+int factZeroCount(int num){
+    int fact = 1;
+    for(int i=2; i<=num; i++){
+        fact = fact * i;
+    }
+    int countZero = 0;
+    while(fact % 10 == 0){
+        countZero++;
+        fact = fact / 10;
+    }
+    return countZero;
+}
+
 int factorial(int num){
     int fact = 1;
     for(int i=1; i<=num; i++){
@@ -17,8 +30,8 @@ int factorial(int num){
 }
 
 int main(){
-    int num = 5;
-    //cout<<factorial(num);
-
-    cout<<recursionFactorial(num);
+    int num = 10;
+    // cout<<factorial(num);
+    // cout<<recursionFactorial(num);
+    cout<<factZeroCount(num);
 }
