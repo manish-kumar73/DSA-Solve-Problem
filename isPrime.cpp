6b8f4fe4f96cc180b54e8d7cedc63e48633d94c7
manish kumar <manishkumarr2060@gmail.com>
2025-07-10 11:44:22 +0530

    add:Prime:Number:code

---
+++ isPrime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n == 1){
+        return false;
+    }
+    if(n == 2 || n == 3){
+        return true;
+    }
+    if(n%2==0 || n%3==0){
+        return false;
+    }
+    for(int i=2 ; i*i <= n; i=i+6){
+        if(n%i == 0 || n%(i+2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+   int n = 56;
+   if(isPrime(n)){
+       cout<<"prime";
+   }else{
+       cout<<"No Prime";
+   }
+}
